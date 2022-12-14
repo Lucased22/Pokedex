0a73721b3cce6584ed7dde2be2cc299041fe896c
Lucased22 <lucaseduardo.siq@gmail.com>
2022-12-14 15:46:05 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Lucas Santos on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
